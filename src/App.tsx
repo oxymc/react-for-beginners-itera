ca652aff7c26704a14e95085892bfed4cad2cac5
Maxym Balagur <maxymbalagur@MacBook-Pro-Maxym.local>
2022-05-02 09:24:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+import { useState } from 'react';
+import { TestComponent } from './components/TestComponent'
 import './App.css';
 
 function App() {
+  const [name, setName] = useState<string | null>('')
+  const [lastName, setLastName] = useState<string | null>('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TestComponent 
+        name = {name}
+        lastName={lastName}
+        getFirstName={() => setName(prompt("What's your name?"))}
+        getLastName={() => setLastName(prompt("What's your last name?"))}
+      />
+    </>
   );
 }
 
